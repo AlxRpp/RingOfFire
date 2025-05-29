9b802ec9f80912c769ae28ba88aee228c7b7cb35
AlxRpp <alexanderr26@outlook.de>
2025-05-29 17:12:22 +0200

    added card stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+
+  public game: Game
   animationPlayed = false;
+
+
+  constructor() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
+
+
 
   takeCard() {
     this.animationPlayed = true;
