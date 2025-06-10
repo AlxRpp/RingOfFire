73a971a6488dc67a6df0e722ef2e565f7ddf0bb1
AlxRpp <alexanderr26@outlook.de>
2025-06-10 19:21:58 +0200

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-37737","appId":"1:912636157058:web:ce31062675f16f2a02a89b","storageBucket":"ringoffire-37737.firebasestorage.app","apiKey":"AIzaSyDvPXEe6THRgC2lgwxEpm31jFvVLZecchA","authDomain":"ringoffire-37737.firebaseapp.com","messagingSenderId":"912636157058"})), provideFirestore(() => getFirestore())]
 };
